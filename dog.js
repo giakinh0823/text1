bf393f39b93151e195157045fba8a87fd7c41be2
giakinh <giakinh2000@gmail.com>
2020-02-26 22:42:53 +0700

    Say Hi im tom

--- dog.js
+++ dog.js
@@ -1,6 +1,15 @@
-function dog() {
-	
+
+var chalk=require('chalk');
+
+function dog(name) {
+	this.name=name;
+	this.stomach=[];
 }
 dog.prototype.dog = function(cat) {
 	this.stomach.push(cat);
 };
+dog.prototype.sayHi=function(){
+	console.log('Hi! I am dog. My name is '+ chalk.blue(this.name));
+};
+
+module.exports=dog;
